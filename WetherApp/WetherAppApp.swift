dd06c11695bdd6a35dd0e22f13982267aeefdc21
martin-ngigi <martinwainaina001@gmail.com>
2023-11-07 20:53:42 +0300

    Initial commit

---
+++ WetherApp/WetherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WetherAppApp.swift
+//  WetherApp
+//
+//  Created by Martin Wainaina on 07/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct WetherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
